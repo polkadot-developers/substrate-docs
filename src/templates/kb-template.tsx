100de4df5c44b125423eac6f79b4b873cd12d9af
Imad Arain <arain.imad@gmail.com>
2021-06-29 16:57:31 +0200

    Finalized Internationalization for docs Menu

--- src/templates/kb-template.tsx
+++ src/templates/kb-template.tsx
@@ -3,15 +3,86 @@ import { graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from '../components/Layout'
 import SEO from '../components/SEO'
-import { kbSideBar } from '../sidebar/kbSideBar'
-import { globalDocsNav } from '../sidebar/globalDocsNav'
+import { useIntl } from 'react-intl'
 import DocsNavMobile from '../components/DocsNavMobile'
 import DocsSideBar from '../components/DocsSideBar'
 import DocsNav from '../components/DocsNav'
 import VersionControl from '../components/VersionControl'
 
-const BlogTemplate = ({ data, pageContext }) => {
+const DocsTemplate = ({ data, pageContext }) => {
   const { slug, version } = pageContext
+  const intl = useIntl()
+  const docsMenu = [
+    {
+      name: `${intl.formatMessage({ id: 'docs-menu-getting-started' })}`,
+      items: [
+        {
+          title: `${intl.formatMessage({ id: 'docs-menu-overview' })}`,
+          link: '/v3/docs/',
+        },
+        {
+          title: `${intl.formatMessage({ id: 'docs-menu-architecture' })}`,
+          link: '/v3/docs/knowledgebase/getting-started/architecture',
+        },
+        {
+          title: `${intl.formatMessage({ id: 'docs-menu-installation' })}`,
+          link: '/v3/docs/knowledgebase/getting-started/',
+        },
+        {
+          title: `${intl.formatMessage({ id: 'docs-menu-windows' })}`,
+          link: '/v3/docs/knowledgebase/getting-started/windows-users',
+        },
+        {
+          title: `${intl.formatMessage({ id: 'docs-menu-glossary' })}`,
+          link: '/v3/docs/knowledgebase/getting-started/glossary',
+        },
+      ],
+    },
+    {
+      name: `${intl.formatMessage({ id: 'docs-menu-key-concepts' })}`,
+      items: [
+        {
+          title: `${intl.formatMessage({ id: 'docs-menu-runtime' })}`,
+          link: '/v3/docs/knowledgebase/runtime',
+        },
+        {
+          title: `${intl.formatMessage({ id: 'docs-menu-extrinsics' })}`,
+          link: '/v3/docs/knowledgebase/learn-substrate/extrinsics',
+        },
+        {
+          title: `${intl.formatMessage({
+            id: 'docs-menu-account-abstractions',
+          })}`,
+          link: '/v3/docs/knowledgebase/learn-substrate/account-abstractions',
+        },
+        {
+          title: `${intl.formatMessage({ id: 'docs-menu-transaction-pool' })}`,
+          link: '/v3/docs/knowledgebase/learn-substrate/learn-substrate/tx-pool',
+        },
+        {
+          title: `${intl.formatMessage({ id: 'docs-menu-session-keys' })}`,
+          link: '/v3/docs/knowledgebase/learn-substrate/learn-substrate/session-keys',
+        },
+        {
+          title: `${intl.formatMessage({
+            id: 'docs-menu-transaction-weight',
+          })}`,
+          link: '/v3/docs/knowledgebase/learn-substrate/learn-substrate/weight',
+        },
+        {
+          title: `${intl.formatMessage({ id: 'docs-menu-offchain-features' })}`,
+          link: '/v3/docs/knowledgebase/learn-substrate/off-chain-features',
+        },
+      ],
+    },
+  ]
+  const globalDocsNav = [
+    `${intl.formatMessage({ id: 'docs-nav-knowledgebase' })}`,
+    `${intl.formatMessage({ id: 'docs-nav-tutorials' })}`,
+    `${intl.formatMessage({ id: 'docs-nav-htg' })}`,
+    `${intl.formatMessage({ id: 'docs-nav-rustdocs' })}`,
+    `${intl.formatMessage({ id: 'docs-nav-learningtracks' })}`,
+  ]
   return (
     <Layout>
       <SEO title={data.mdx ? data.mdx.frontmatter.title : null} />
@@ -19,14 +90,14 @@ const BlogTemplate = ({ data, pageContext }) => {
         <div className="flex flex-col lg:container lg:flex-row ">
           <div className="lg:hidden">
             <DocsNavMobile
-              sideNav={kbSideBar}
+              sideNav={docsMenu}
               globalNav={globalDocsNav}
               templateId={'Knowledgebase'}
             />
           </div>
           <div className="hidden lg:inline-block lg:flex-none">
             <DocsNav
-              sideNav={kbSideBar}
+              sideNav={docsMenu}
               globalNav={globalDocsNav}
               templateId={'Knowledgebase'}
             />
@@ -58,7 +129,7 @@ const BlogTemplate = ({ data, pageContext }) => {
   )
 }
 
-export default BlogTemplate
+export default DocsTemplate
 
 export const query = graphql`
   query ($locale: String!, $slug: String!) {
