0521d57c351d61199e6d4a904534acb0af3c58e8
Imad Arain <arain.imad@gmail.com>
2021-07-12 12:28:43 +0200

    Activating Tutorials Templates

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,6 +6,21 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
   const tutorialOneTemplate = require.resolve(
     `./src/templates/tut-templates/create-your-first-substrate-chain.tsx`
   )
+  const tutorialTwoTemplate = require.resolve(
+    `./src/templates/tut-templates/proof-of-existence.tsx`
+  )
+  const tutorialThreeTemplate = require.resolve(
+    `./src/templates/tut-templates/permissioned-network.tsx`
+  )
+  const tutorialFourTemplate = require.resolve(
+    `./src/templates/tut-templates/forkless-upgrade.tsx`
+  )
+  const tutorialFiveTemplate = require.resolve(
+    `./src/templates/tut-templates/private-network.tsx`
+  )
+  const tutorialSixTemplate = require.resolve(
+    `./src/templates/tut-templates/node-metrics.tsx`
+  )
 
   const result = await graphql(`
     {
@@ -40,6 +55,17 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
           }
         }
       }
+      tutorialTwo: allFile(
+        filter: { sourceInstanceName: { eq: "proof-of-existence" } }
+      ) {
+        nodes {
+          childMdx {
+            frontmatter {
+              slug
+            }
+          }
+        }
+      }
       tutorialThree: allFile(
         filter: { sourceInstanceName: { eq: "permissioned-network" } }
       ) {
@@ -64,6 +90,17 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
       }
       tutorialFive: allFile(
         filter: { sourceInstanceName: { eq: "private-network" } }
+      ) {
+        nodes {
+          childMdx {
+            frontmatter {
+              slug
+            }
+          }
+        }
+      }
+      tutorialSix: allFile(
+        filter: { sourceInstanceName: { eq: "node-metrics" } }
       ) {
         nodes {
           childMdx {
@@ -110,6 +147,63 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     createPage({
       path: `${node.frontmatter.slug}`,
       component: tutorialOneTemplate,
+      context: {
+        slug: `${node.frontmatter.slug}`,
+        version: `3.0`,
+      },
+    })
+  })
+
+  const tutorialTwo = result.data.tutorialTwo.nodes
+  tutorialTwo.forEach(({ childMdx: node }) => {
+    createPage({
+      path: `${node.frontmatter.slug}`,
+      component: tutorialTwoTemplate,
+      context: {
+        slug: `${node.frontmatter.slug}`,
+        version: `3.0`,
+      },
+    })
+  })
+
+  const tutorialThree = result.data.tutorialThree.nodes
+  tutorialThree.forEach(({ childMdx: node }) => {
+    createPage({
+      path: `${node.frontmatter.slug}`,
+      component: tutorialThreeTemplate,
+      context: {
+        slug: `${node.frontmatter.slug}`,
+        version: `3.0`,
+      },
+    })
+  })
+  const tutorialFour = result.data.tutorialFour.nodes
+  tutorialFour.forEach(({ childMdx: node }) => {
+    createPage({
+      path: `${node.frontmatter.slug}`,
+      component: tutorialFourTemplate,
+      context: {
+        slug: `${node.frontmatter.slug}`,
+        version: `3.0`,
+      },
+    })
+  })
+  const tutorialFive = result.data.tutorialFive.nodes
+  tutorialFive.forEach(({ childMdx: node }) => {
+    createPage({
+      path: `${node.frontmatter.slug}`,
+      component: tutorialFiveTemplate,
+      context: {
+        slug: `${node.frontmatter.slug}`,
+        version: `3.0`,
+      },
+    })
+  })
+  const tutorialSix = result.data.tutorialSix.nodes
+  tutorialSix.forEach(({ childMdx: node }) => {
+    createPage({
+      path: `${node.frontmatter.slug}`,
+      component: tutorialSixTemplate,
       context: {
         slug: `${node.frontmatter.slug}`,
         version: `3.0`,
