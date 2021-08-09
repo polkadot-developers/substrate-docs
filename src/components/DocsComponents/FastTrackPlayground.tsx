93eb618e8bd346475d91764e135146b28a87c59d
Imad Arain <arain.imad@gmail.com>
2021-08-09 16:28:34 +0200

    Updating mdx buttons to reflect primary and secondary styling

--- src/components/DocsComponents/FastTrackPlayground.tsx
+++ src/components/DocsComponents/FastTrackPlayground.tsx
@@ -5,36 +5,29 @@ interface FastTrackPlaygroundProps {
   title: string
   description: string
   playgroundLink: string
+  header: string
 }
 
 export function FastTrackPlayground({
   title,
+  header,
   description,
   playgroundLink,
 }: FastTrackPlaygroundProps) {
   return (
     <>
-      <div className="max-w-sm rounded-lg border border-gray-200">
-        <div className="flex items-center px-4 py-4 rounded-t-lg bg-green-200">
-          <svg
-            className="fill-current text-black"
-            width="18"
-            height="18"
-            viewBox="0 0 18 18"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M9 0C4.03106 0 0 4.03106 0 9C0 13.9689 4.03106 18 9 18C13.9689 18 18 13.9689 18 9C18 4.03106 13.9689 0 9 0ZM9 16.1964C5.03206 16.1964 1.80361 12.9679 1.80361 9C1.80361 5.03206 5.03206 1.80361 9 1.80361C12.9679 1.80361 16.1964 5.03206 16.1964 9C16.1964 12.9679 12.9679 16.1964 9 16.1964ZM9.67635 2.68738L5.61824 10.3527H8.4499V15.3126L12.3818 7.64729H9.67635V2.68738Z" />
-          </svg>
-          <span className="text-sm pl-2 font-medium dark:text-black">
-            Launch a Node Template From Your Browser
+      <div className="max-w-sm rounded bg-substrateGray-light">
+        <div className="px-6 py-3 rounded-t-lg bg-substrateGreen">
+          <span className="text-sm text-white font-bold dark:text-black">
+            {header}
           </span>
         </div>
-        <div className="px-4 pb-4">
+        <div className="px-6 pb-4">
           <h3>{title}</h3>
           <p className="pb-2">{description}</p>
           <LocalizedLink className="mx-auto" to={playgroundLink}>
-            <div className="flex items-center justify-center py-2 rounded-lg bg-black dark:bg-gray-800 hover:no-underline">
-              <button className="text-lg text-white font-bold focus:outline-none hover:no-underline">
+            <div className="flex items-center justify-center w-full h-14 rounded bg-substrateDark transform transition duration-300 ease-in-out hover:bg-opacity-90">
+              <button className="text-lg text-white font-bold focus:outline-none">
                 Set up Playground
               </button>
             </div>
