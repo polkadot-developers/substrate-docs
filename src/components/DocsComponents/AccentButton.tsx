03d46511f3a4b59c8d584849e42cae98c10c057e
Imad Arain <arain.imad@gmail.com>
2021-08-09 18:05:54 +0200

    Bottom Button adjustments plus style updates

--- src/components/DocsComponents/AccentButton.tsx
+++ src/components/DocsComponents/AccentButton.tsx
@@ -10,7 +10,7 @@ export function AccentButton({ text, link }: AccentButtonProps) {
     <>
       <LocalizedLink className="md-button group inline-block my-6" to={link}>
         <div className="flex items-center justify-center w-60 h-14 rounded bg-substrateGreen transform transition duration-300 ease-in-out group-hover:bg-white border-2 border-transparent group-hover:border-substrateGreen">
-          <button className="text-lg text-white group-hover:text-substrateGreen dark:text-substrateDark font-bold pr-4 focus:outline-none">
+          <button className="text-lg text-white group-hover:text-substrateGreen dark:text-substrateDark font-bold focus:outline-none">
             {text}
           </button>
         </div>
