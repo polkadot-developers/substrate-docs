67d4d006ab1ef60e1c0ca2cc200f3d0c91c58667
Imad Arain <arain.imad@gmail.com>
2021-08-22 13:07:14 +0200

    Web Review updates for styling - padding, responsive behavior, etc

--- src/components/SearchDocs.tsx
+++ src/components/SearchDocs.tsx
@@ -125,12 +125,12 @@ export default function SearchDocs() {
           >
             <div
               ref={ref}
-              className="bg-white dark:bg-gray-900 w-full max-w-screen-sm h-auto p-6 rounded-lg border-2 border-substrateDark shadow-xl"
+              className="bg-white dark:bg-gray-900 w-full max-w-screen-sm h-auto pt-12 pb-20 px-8 rounded-lg border-2 border-substrateDark shadow-xl"
             >
               <div className="flex justify-between mb-6">
                 <div className="flex items-center justify-between w-5/6 pb-0.5 border-b-2 border-substrateGray">
                   <svg
-                    className="h-4 w-4 fill-current text-substrateDark dark:text-white"
+                    className="h-4 w-4 md:h-6 md:w-6 fill-current text-substrateDark dark:text-white"
                     xmlns="http://www.w3.org/2000/svg"
                     version="1.1"
                     id="Capa_1"
@@ -143,7 +143,7 @@ export default function SearchDocs() {
                     <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
                   </svg>
                   <input
-                    className="form-input w-full pl-4 text-lg border-none dark:bg-gray-900 focus:ring-0 cursor-text"
+                    className="form-input w-full pl-4 text-lg md:text-2xl border-none dark:bg-gray-900 focus:ring-0 cursor-text"
                     type="text"
                     value={query}
                     placeholder="Search Documentation"
@@ -207,7 +207,7 @@ export default function SearchDocs() {
                       ))}
                     </div>
                   ) : (
-                    <div className="overflow-auto pr-4">
+                    <div className="overflow-auto">
                       {searchResults.length > 0 ? (
                         <div>
                           {displayedResults.map((result, index) => {
--- src/components/SearchSectionLabel.tsx
+++ src/components/SearchSectionLabel.tsx
@@ -53,12 +53,14 @@ export default function SearchSectionLabel({
         }`}
       >
         <input
-          className="h-4 w-4 rounded bg-white text-substrateDark border-2 focus:ring-0 "
+          className="h-4 w-4 md:h-[18px] md:w-[18px] rounded bg-white text-substrateDark border-2 focus:ring-0 "
           type="checkbox"
           checked={checked() || false}
           onChange={() => handleChange()}
         />
-        <span className="text-base font-semibold pl-2">{children}</span>
+        <span className="text-base md:text-lg font-semibold pl-2">
+          {children}
+        </span>
       </label>
     </>
   )
