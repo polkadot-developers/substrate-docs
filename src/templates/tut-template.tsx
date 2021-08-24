aff3fd7ea94b725c9bbbd30568d0f0927e28cf44
Imad Arain <arain.imad@gmail.com>
2021-08-24 17:21:25 +0200

    Added frontmatter to tutorials, and updated layout to display difficulty and time

--- src/templates/tut-template.tsx
+++ src/templates/tut-template.tsx
@@ -40,12 +40,47 @@ const DocsTemplate = ({ data, pageContext }: any) => {
             <div>
               <div>
                 {data.mdx ? (
-                  <div className="pt-10">
-                    <VersionControl
-                      version={version}
-                      slug={slug}
-                      absolutePath={data.mdx.fileAbsolutePath}
-                    />
+                  <div>
+                    <div
+                      className={`py-8 ${
+                        data.mdx.frontmatter.difficulty &&
+                        'flex justify-between'
+                      }`}
+                    >
+                      {data.mdx.frontmatter.difficulty && (
+                        <div className="inline-block text-xs">
+                          <button className="bg-substrateBlue bg-opacity-5 px-4 py-2 border border-substratePurple border-opacity-30 rounded cursor-text">
+                            {data.mdx.frontmatter.difficulty === 1
+                              ? 'Beginner'
+                              : data.mdx.frontmatter.difficulty === 2
+                              ? 'Intermediate'
+                              : data.mdx.frontmatter.difficulty === 3
+                              ? 'Advanced'
+                              : null}
+                          </button>
+                          <button className="bg-substrateBlue bg-opacity-5 ml-4 px-4 py-2 border border-substratePurple border-opacity-30 rounded cursor-text">
+                            <svg
+                              className="inline-block -mt-0.5 fill-current text-black dark:text-white"
+                              width="14"
+                              height="14"
+                              viewBox="0 0 14 14"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path d="M7.02622 0C10.8539 0 14 3.14607 14 7.02622C14 10.8539 10.8539 14 7.02622 14C3.14607 14 0 10.8539 0 7.02622C0 3.14607 3.14607 0 7.02622 0ZM6.29213 3.1985C6.29213 2.51685 7.34082 2.51685 7.34082 3.1985V6.92135L9.75281 7.603C10.382 7.81273 10.1199 8.80899 9.4382 8.65168L6.71161 7.81273C6.44944 7.7603 6.29213 7.55056 6.29213 7.28839V3.1985ZM7.02622 1.04869C3.72285 1.04869 1.04869 3.72285 1.04869 7.02622C1.04869 10.2772 3.72285 12.9513 7.02622 12.9513C10.2772 12.9513 12.9513 10.2772 12.9513 7.02622C12.9513 3.72285 10.2772 1.04869 7.02622 1.04869Z" />
+                            </svg>
+                            <span className="ml-2">
+                              {data.mdx.frontmatter.duration}
+                            </span>
+                          </button>
+                        </div>
+                      )}
+                      <VersionControl
+                        version={version}
+                        slug={slug}
+                        absolutePath={data.mdx.fileAbsolutePath}
+                      />
+                    </div>
+
                     <div className="markdown-body mdx-anchor">
                       <h1>{data.mdx.frontmatter.title}</h1>
                       <MDXRenderer>{data.mdx.body}</MDXRenderer>
@@ -119,6 +154,8 @@ export const query = graphql`
         slug
         title
         hideNav
+        difficulty
+        duration
       }
       body
       headings {
@@ -136,6 +173,8 @@ export const query = graphql`
         slug
         title
         hideNav
+        difficulty
+        duration
       }
       body
       headings {
