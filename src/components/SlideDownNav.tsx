33964f9c3dfc95ec57c120691bb6e4d387c00b8e
Imad Arain <arain.imad@gmail.com>
2021-09-01 10:14:55 +0200

    Refactored DocsSideBar

--- src/components/SlideDownNav.tsx
+++ src/components/SlideDownNav.tsx
@@ -18,11 +18,13 @@ export default function SlideDownNav({ section }: SlideDownNavProps) {
     <>
       <div
         onClick={() => setIsOpen(!isOpen)}
-        className="flex items-center justify-between px-20 lg:px-4 py-4 bg-substrateGray-light dark:bg-gray-900 cursor-pointer"
+        className="flex items-center justify-between px-20 lg:px-6 py-3 bg-substrateGray-light dark:bg-gray-900 cursor-pointer"
       >
-        <div className="font-medium">{section.name}</div>
+        <div className={`${isOpen ? `font-bold` : `font-medium`}`}>
+          {section.name}
+        </div>
         <svg
-          className={`fill-current text-black dark:text-white transform ease-in-out ${
+          className={`fill-current text-black dark:text-white ${
             isOpen ? '-rotate-180' : null
           }`}
           xmlns="http://www.w3.org/2000/svg"
@@ -39,13 +41,13 @@ export default function SlideDownNav({ section }: SlideDownNavProps) {
           section.items.map((item, index) => {
             return (
               <LocalizedLink
-                className="hover:no-underline"
+                className="hover:no-underline "
                 key={index}
                 to={item.link}
               >
                 <div
-                  className={`text-gray-600 dark:text-gray-200 text-sm px-20 lg:px-6 py-2 hover:font-bold ${
-                    item.link === location.pathname ? `font-bold` : ` `
+                  className={`text-gray-600 dark:text-gray-200 text-sm px-20 lg:pl-10 lg:pr-6 py-2 hover:text-shadow ${
+                    item.link === location.pathname ? `text-shadow` : ` `
                   }`}
                 >
                   {item.title}
