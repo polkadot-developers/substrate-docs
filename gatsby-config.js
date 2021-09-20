661269cfd68be215615b56861b78de85f99354a3
Sacha Lansky <sacha@parity.io>
2021-09-20 15:07:10 +0200

    yarn format

--- gatsby-config.js
+++ gatsby-config.js
@@ -130,20 +130,20 @@ module.exports = {
         name: `cumulus`,
       },
     },
-      {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-          path: `${__dirname}/v3/tutorials/10-frontier-workshop/`,
-          name: `frontier`,
-        },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/v3/tutorials/10-frontier-workshop/`,
+        name: `frontier`,
       },
-      {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-          path: `${__dirname}/v3/tutorials/11-kitties-workshop/`,
-          name: `kitties`,
-        },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/v3/tutorials/11-kitties-workshop/`,
+        name: `kitties`,
       },
+    },
     //*********************************//
     {
       resolve: `gatsby-source-filesystem`,
--- src/components/DevNavMenu.tsx
+++ src/components/DevNavMenu.tsx
@@ -405,6 +405,10 @@ const DevNavMenu = {
             title: `${intl.formatMessage({ id: 'htg-consensus-pow' })}`,
             link: '/how-to-guides/v3/consensus/pow',
           },
+          {
+            title: `${intl.formatMessage({ id: 'htg-consensus-hybrid' })}`,
+            link: '/how-to-guides/v3/consensus/hybrid-pos-pow',
+          },
         ],
       },
       // 7. Parachains.
@@ -636,22 +640,22 @@ DevNavMenu.tuts.addPallet = [
 
 DevNavMenu.tuts.inkWorkshop = [
   {
-  name: `ink! Contracts Workshop`,
-  items: [
-    {
-      title: `Getting Started`,
-      link: '/tutorials/v3/ink-workshop/pt1',
-    },
-    {
-      title: `Develop a Smart Contract`,
-      link: '/tutorials/v3/ink-workshop/pt2',
-    },
-    {
-      title: `Build an ERC20 Token Contract`,
-      link: '/tutorials/v3/ink-workshop/pt3',
-    },
-  ],
-},
+    name: `ink! Contracts Workshop`,
+    items: [
+      {
+        title: `Getting Started`,
+        link: '/tutorials/v3/ink-workshop/pt1',
+      },
+      {
+        title: `Develop a Smart Contract`,
+        link: '/tutorials/v3/ink-workshop/pt2',
+      },
+      {
+        title: `Build an ERC20 Token Contract`,
+        link: '/tutorials/v3/ink-workshop/pt3',
+      },
+    ],
+  },
 ]
 
 DevNavMenu.tuts.cumulusWorkshop = [
