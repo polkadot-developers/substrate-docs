d8fb3ad49eb4e419b9ef2339f18a04ebd72b05a8
Imad Arain <arain.imad@gmail.com>
2021-09-30 13:12:52 +0200

    playground page, subtext update

--- src/pages/playground.tsx
+++ src/pages/playground.tsx
@@ -21,7 +21,20 @@ export default function playground() {
       <section className="xl:container my-20 md:mt-20 md:mb-36">
         <div className="flex flex-col md:flex-row md:items-center px-6">
           <div className="lg:m-0 md:w-1/2 md:px-5 lg:px-10">
-            <div className="text-5xl xl:text-7xl font-title font-extrabold mb-6">
+            <div className="font-title text-5xl lg:text-6xl font-extrabold mb-8">
+              Playground
+            </div>
+            <h4 className="text-xl xl:text-3xl font-semibold">
+              Set up a cloud sandbox
+            </h4>
+            <p className="max-w-lg leading-7">
+              We recommend picking a sandbox, particularly if you&apos;re just
+              getting started. Building on Substrate requires different pieces
+              of technology. Useful for hosting Substrate developer workshops or
+              following tutorials, using a sandbox allows you to skip the
+              preliminary set-up to get to the parts most important to you.
+            </p>
+            {/* <div className="text-5xl xl:text-7xl font-title font-extrabold mb-6">
               Playground
             </div>
             <h4 className="text-xl font-semibold">
@@ -33,7 +46,7 @@ export default function playground() {
               of technology. Using a sandbox allows you to skip preliminary
               set-up and prerequisites to get to the parts that are important to
               you.
-            </p>
+            </p> */}
           </div>
           <div className="flex justify-center md:w-1/2">
             <StaticImage
