c38b192d8b92cd12457a25c07d1f39cb6c6dbd87
Imad Arain <arain.imad@gmail.com>
2021-09-30 16:34:49 +0200

    breadcrumb arrows update for dark mode

--- src/components/BreadCrumbNav.tsx
+++ src/components/BreadCrumbNav.tsx
@@ -11,9 +11,9 @@ export default function BreadCrumbNav(props: BreadCrumbNavProps) {
     <>
       <div className="text-sm font-medium text-substrateBlue dark:text-substrateBlue-light capitalize mdx-anchor">
         <LocalizedLink to="/">Developers Home</LocalizedLink>
-        <span className="px-2 text-substrateDark">»</span>
+        <span className="px-2 text-substrateDark dark:text-white">»</span>
         <LocalizedLink to={props.sectionURL}>{props.section}</LocalizedLink>
-        <span className="px-2 text-substrateDark">»</span>
+        <span className="px-2 text-substrateDark dark:text-white">»</span>
         <span className="text-substrateDark dark:text-white">
           {props.title}
         </span>
