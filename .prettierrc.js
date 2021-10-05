ab69f74830f91f280881faf57d00268f12bb954e
Dan Shields <nukemandan@protonmail.com>
2021-10-05 10:46:45 -0600

    up

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-  arrowParens: 'avoid',
-  endOfLine: 'lf',
-  semi: false,
-  singleQuote: true,
-  trailingComma: 'es5',
-}
+    arrowParens: 'avoid',
+    endOfLine: 'lf',
+    semi: false,
+    singleQuote: true,
+    trailingComma: 'es5',
+    printWidth: '100',
+    tabWidth: '2',
+};
