509ac269c8e5df1bd6f4fda55035db564b99f774
Imad Arain <arain.imad@gmail.com>
2021-10-06 17:35:45 +0200

    updating body text

--- src/components/DocsComponents/FeedbackWidget.tsx
+++ src/components/DocsComponents/FeedbackWidget.tsx
@@ -10,17 +10,7 @@ export function FeedbackWidget({ title, slug, section }: FeedbackWidgetProps) {
   const githubLink =
     'https://github.com/substrate-developer-hub/substrate-docs/issues/new?'
   const githubTitle = `Feedback On - ${section}/${title}`
-  const body = `**Symptom**
-  Describe what you saw or experienced. Did something go wrong or do you have a suggestion for improving our site?
-  
-  **Expected behavior**
-  Describe what you expected to see or to have happened. Or if something delighted you, we'd love to know that, too.
-  
-  **Steps to reproduce**
-  Provide the steps that led to the discovery of the issue. If something went wrong, we want to fix it!
-  
-  **Recommended course of action (optional)**
-  If you have a suggestion for fixing the issue you found, describe what you think the fix should be.`
+  const body = `Feedback body - hey I can also insert \`${slug}\` here ;-)`
   return (
     <div>
       <h4 className="pt-10">Was This Page Helpful?</h4>
