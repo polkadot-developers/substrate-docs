bf082fe6c243994cc0933f306773644dc0556c66
Sacha Lansky <sacha@parity.io>
2021-10-07 16:16:27 +0200

    WIP Add new benchmarking htgs

--- src/components/DevNavMenu.tsx
+++ src/components/DevNavMenu.tsx
@@ -378,6 +378,18 @@ const DevNavMenu = {
             })}`,
             link: '/how-to-guides/v3/weights/calculate-fees',
           },
+          {
+            title: `${intl.formatMessage({
+              id: 'htg-weights-add-benchmarking',
+            })}`,
+            link: '/how-to-guides/v3/weights/benchmarking',
+          },
+          {
+            title: `${intl.formatMessage({
+              id: 'htg-weights-use-benchmark-weights',
+            })}`,
+            link: '/how-to-guides/v3/weights/use-benchmark-weights',
+          },
         ],
       },
       // 4. Testing.
