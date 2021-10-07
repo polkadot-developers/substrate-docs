e4dd6dd32a104470974393f71b0f93963ce67a45
Imad Arain <arain.imad@gmail.com>
2021-10-07 17:34:30 +0200

    activating component on all templates

--- src/components/DocsComponents/FeedbackWidget.tsx
+++ src/components/DocsComponents/FeedbackWidget.tsx
@@ -10,41 +10,35 @@ interface FeedbackWidgetProps {
 export function FeedbackWidget({ title, slug, section }: FeedbackWidgetProps) {
   const githubLink =
     'https://github.com/substrate-developer-hub/substrate-docs/issues/new?'
-  const githubTitle = `Feedback Submission - ${section}/${title}`
+  const githubTitle = `Feedback Submission - ${section} / ${title}`
+  console.log(section)
+  const currentSection = () => {
+    if (section === 'how to guides') {
+      return 'Guide'
+    }
+    if (section === 'tutorials') {
+      return 'Tutorial'
+    }
+    return 'Page'
+  }
   return (
     <div>
       <div className="flex items-center pt-10 mb-4">
-        <span className="text-xl font-semibold">Was This Page Helpful?</span>
+        <Icon
+          name="feedbackIcon"
+          className="mr-2 fill-current text-subtrateDark dark:text-substrateWhite"
+        />
+        <span className="text-xl font-semibold">
+          Was This {currentSection()} Helpful?
+        </span>
       </div>
 
       <SecondaryButton
         external
         link={`${githubLink}title=${githubTitle}&template=feedback-template.md&labels=feedback`}
       >
-        <div className="flex items-center">
-          <Icon
-            name="feedbackIcon"
-            className="mr-2 fill-current text-subtrateDark dark:text-substrateWhite"
-          />
-          Help us improve
-        </div>
+        Help us improve
       </SecondaryButton>
     </div>
-    // <div>
-    //   <div className="flex items-center pt-10 mb-4">
-    //     <Icon
-    //       name="feedbackIcon"
-    //       className="mr-2 fill-current text-subtrateDark dark:text-substrateWhite"
-    //     />
-    //     <span className="text-xl font-semibold">Was This Page Helpful?</span>
-    //   </div>
-
-    //   <SecondaryButton
-    //     external
-    //     link={`${githubLink}title=${githubTitle}&body=${body}&labels=feedback`}
-    //   >
-    //     Help us improve
-    //   </SecondaryButton>
-    // </div>
   )
 }
