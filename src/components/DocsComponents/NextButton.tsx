c42fb8e11479c6cdfb19fcd6f2c05c4553e2ac8f
joneirikboska <joneirikboska@gmail.com>
2021-10-11 16:28:17 +0200

    Set NextButton active state text color white

--- src/components/DocsComponents/NextButton.tsx
+++ src/components/DocsComponents/NextButton.tsx
@@ -12,8 +12,8 @@ export function NextButton({ text, link }: NextButtonProps) {
     <>
       <Link to={link}>
         <div className="md-button group inline-block my-6">
-          <div className="flex items-center justify-center w-80 h-14 rounded bg-substrateGreen transform transition duration-300 ease-in-out group-hover:bg-white dark:group-hover:bg-darkBackground border-2 border-transparent group-hover:border-substrateGreen">
-            <button className="truncate text-lg text-white dark:text-white px-4 group-hover:text-substrateGreen font-bold focus:outline-none">
+          <div className="flex items-center justify-center w-80 h-14 rounded bg-substrateGreen transform transition duration-300 ease-in-out group-active:bg-white group-hover:bg-white dark:group-hover:bg-darkBackground border-2 border-transparent group-hover:border-substrateGreen">
+            <button className="truncate text-lg text-white dark:text-white px-4 group-hover:text-substrateGreen font-bold focus:outline-none group-active:text-white">
               {intl.formatMessage({ id: 'docs-nav-next' })} - {text}
             </button>
           </div>
