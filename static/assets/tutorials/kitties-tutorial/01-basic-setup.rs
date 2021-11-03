0e24c562f55c1c1699fbe30cc2aec9bce6a4be26
Dan Shields <nukemandan@protonmail.com>
2021-11-03 18:53:41 +0100

    back to `pub struct Pallet<T>(_);`

--- static/assets/tutorials/kitties-tutorial/01-basic-setup.rs
+++ static/assets/tutorials/kitties-tutorial/01-basic-setup.rs
@@ -18,7 +18,7 @@ pub mod pallet {
     #[pallet::pallet]
     #[pallet::generate_store(pub(super) trait Store)]
     #[pallet::generate_storage_info]
-    pub struct Pallet<T, I = ()>(PhantomData<(T, I)>);
+    pub struct Pallet<T>(_);
 
     /// Configure the pallet by specifying the parameters and types it depends on.
     #[pallet::config]
