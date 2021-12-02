b4b40af7208a097f3c81e76a9924042fbca5b79a
Imad Arain <arain.imad@gmail.com>
2021-12-02 18:21:43 +0100

    adding isBrowser() function to check for DOM

--- src/components/Hooks/use-sessions-storage.tsx
+++ src/components/Hooks/use-sessions-storage.tsx
@@ -1,7 +1,11 @@
 import { useState, useEffect } from 'react'
 
+function isBrowser() {
+  return typeof window !== 'undefined'
+}
+
 function getSessionStorageOrDefault(key: string, defaultValue: boolean) {
-  const stored = sessionStorage.getItem(key)
+  const stored = isBrowser() && window.sessionStorage.getItem(key)
   if (!stored) {
     return defaultValue
   }
@@ -14,7 +18,7 @@ function useSessionStorage(key: string, defaultValue: boolean) {
   )
 
   useEffect(() => {
-    sessionStorage.setItem(key, JSON.stringify(value))
+    isBrowser() && window.sessionStorage.setItem(key, JSON.stringify(value))
   }, [key, value])
 
   return [value, setValue]
