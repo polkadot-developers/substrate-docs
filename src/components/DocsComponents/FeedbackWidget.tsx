5d11d40701b0326a2eccebc4659b35dbccefa7f6
Dan Shields <nukemandan@protonmail.com>
2021-12-04 13:00:08 -0700

    rm unsused props

--- src/components/DocsComponents/FeedbackWidget.tsx
+++ src/components/DocsComponents/FeedbackWidget.tsx
@@ -2,21 +2,14 @@ import React from 'react'
 import Icon from '../Icon'
 import { SecondaryButton } from '../Buttons'
 
-interface FeedbackWidgetProps {
-  title: string
-  section: string
-  slug: string
-}
-
-export function FeedbackWidget({ title, slug, section }: FeedbackWidgetProps) {
+export function FeedbackWidget() {
   const githubLink =
-    'https://github.com/substrate-developer-hub/substrate-docs/'
-  const githubTitle = `Issue in - ${section} - ${title} - ${slug}`
+    'https://github.com/substrate-developer-hub/substrate-docs/issues/new/choose'
   return (
     <div>
       <div className="flex items-center pt-10 mb-4">
         <Icon
-          name="flag"
+          name="feedbackIcon"
           className="mr-2 fill-current text-substrateDark dark:text-substrateWhite"
         />
         <span className="text-xl font-semibold">
@@ -26,9 +19,9 @@ export function FeedbackWidget({ title, slug, section }: FeedbackWidgetProps) {
 
       <SecondaryButton
         external
-        link={`${githubLink}issues/new/choose?title=${githubTitle}`}
+        link={`${githubLink}`}
       >
-      <div className="flex items-center p-2 m-0">
+      <div className="flex items-center pt-2 mb-2">
         <span className="font-semibold">
           Let us Know
         </span>
