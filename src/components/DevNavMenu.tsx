bf2fda82d1d2a01d9014ff8124b9b8f1d705a29c
Jimmy Chu <jimmychu0807@gmail.com>
2021-12-13 18:49:37 +0800

    Creating the topic structure of off-chain worker

--- src/components/DevNavMenu.tsx
+++ src/components/DevNavMenu.tsx
@@ -350,7 +350,29 @@ const DevNavMenu = {
           },
         ],
       },
-      // 4. Testing.
+      // 4. Off-chain Workers
+      {
+        name: 'Off-chain Workers',
+        items: [
+          {
+            title: 'Making Transactions',
+            link: '/how-to-guides/v3/ocw/transactions/',
+          },
+          {
+            title: 'Making HTTP Requests',
+            link: '/how-to-guides/v3/ocw/http-requests/',
+          },
+          {
+            title: 'Local Storage',
+            link: '/how-to-guides/v3/ocw/local-storage/',
+          },
+          {
+            title: 'Off-chain Indexing',
+            link: '/how-to-guides/v3/ocw/indexing/',
+          }
+        ]
+      },
+      // 5. Testing.
       {
         name: 'Testing',
         items: [
@@ -364,7 +386,7 @@ const DevNavMenu = {
           },
         ],
       },
-      // 5. Storage Migrations.
+      // 6. Storage Migrations.
       {
         name: 'Storage Migrations',
         items: [
@@ -382,7 +404,7 @@ const DevNavMenu = {
           },
         ],
       },
-      // 6. Consensus.
+      // 7. Consensus.
       {
         name: 'Consensus',
         items: [
@@ -396,7 +418,7 @@ const DevNavMenu = {
           },
         ],
       },
-      // 7. Parachains.
+      // 8. Parachains.
       {
         name: 'Parachains',
         items: [
@@ -422,7 +444,7 @@ const DevNavMenu = {
           },
         ],
       },
-      // 8. Tools
+      // 9. Tools
       {
         name: 'Tools',
         items: [
