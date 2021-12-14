9ad006da17754667f6e287f92b203265060c50f8
rmnprkrl <rmnprkrl@gmail.com>
2021-12-14 18:17:44 +0100

    add react-device-detect

--- src/hooks/use-session-storage.js
+++ src/hooks/use-session-storage.js
@@ -1,22 +1,22 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 
-import { isBrowser } from '../utils/browser';
+import { isBrowser } from '../utils/browser'
 
 export const useSessionStorage = (key, initialValue) => {
-  const [isBannerOpen, setIsBannerOpen] = useState(null);
+  const [isBannerOpen, setIsBannerOpen] = useState(null)
 
   useEffect(() => {
-    const stored = isBrowser && window.sessionStorage.getItem(key);
+    const stored = isBrowser && window.sessionStorage.getItem(key)
     if (!stored) {
-      setIsBannerOpen(initialValue);
+      setIsBannerOpen(initialValue)
     } else {
-      setIsBannerOpen(JSON.parse(stored));
+      setIsBannerOpen(JSON.parse(stored))
     }
-  }, []);
+  }, [])
 
   useEffect(() => {
-    window.sessionStorage.setItem(key, JSON.stringify(isBannerOpen));
-  }, [isBannerOpen]);
+    window.sessionStorage.setItem(key, JSON.stringify(isBannerOpen))
+  }, [isBannerOpen])
 
-  return { isBannerOpen, setIsBannerOpen };
-};
+  return { isBannerOpen, setIsBannerOpen }
+}
