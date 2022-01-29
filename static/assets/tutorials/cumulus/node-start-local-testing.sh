98d4ecd3e24f37c191d840b9a92fc79084090863
Dan Shields <nukemandan@protonmail.com>
2022-01-28 22:33:17 -0700

    update to polkadot-v0.9.16

--- static/assets/tutorials/cumulus/node-start-local-testing.sh
+++ static/assets/tutorials/cumulus/node-start-local-testing.sh
@@ -1,3 +1,5 @@
+# NOTE: assumes `substrate-docs` is locally `docs`
+
 # Build Spec
 ./target/release/polkadot build-spec --chain rococo-local --disable-default-bootnode > rococo-custom-2-plain.json
 ./target/release/polkadot build-spec --chain rococo-custom-2-plain.json --raw --disable-default-bootnode > rococo-custom-2-raw.json
@@ -12,11 +14,11 @@
 rm -rf /tmp/relay /tmp/parachain
 
 # Relay Validators with 2 validator chain spec
-./target/release/polkadot --alice --validator --base-path /tmp/relay/alice --chain ../substrate-docs/static/assets/tutorials/cumulus/chain-specs/rococo-custom-2-raw.json --port 30333 --ws-port 9944
+./target/release/polkadot --alice --validator --base-path /tmp/relay/alice --chain ../docs/static/assets/tutorials/cumulus/chain-specs/rococo-custom-2-raw.json --port 30333 --ws-port 9944
 
-./target/release/polkadot --bob --validator --base-path /tmp/relay/bob --chain ../substrate-docs/static/assets/tutorials/cumulus/chain-specs/rococo-custom-2-raw.json --port 30334 --ws-port 9945
+./target/release/polkadot --bob --validator --base-path /tmp/relay/bob --chain ../docs/static/assets/tutorials/cumulus/chain-specs/rococo-custom-2-raw.json --port 30334 --ws-port 9945
 
-# ./target/release/polkadot --charlie --validator --base-path /tmp/relay/charlie --chain ../substrate-docs/static/assets/tutorials/cumulus/chain-specs/rococo-custom-2-raw.json --port 30335 --ws-port 9946
+# ./target/release/polkadot --charlie --validator --base-path /tmp/relay/charlie --chain ../docs/static/assets/tutorials/cumulus/chain-specs/rococo-custom-2-raw.json --port 30335 --ws-port 9946
 
 # Register para ID 2000 with `Charlie`
 # https://polkadot.js.org/apps/?rpc=ws%3A%2F%2F127.0.0.1%3A9944#/parachains/parathreads
@@ -40,7 +42,7 @@ rm -rf /tmp/relay /tmp/parachain
 # Register parathread that charlie reserved (2000)
 
 # Parachain collator launch
-./target/release/parachain-collator --alice --collator --force-authoring --chain rococo-local-parachain-2000-raw.json --base-path /tmp/parachain/alice --port 40333 --ws-port 8844 -- --execution wasm --chain ../substrate-docs/static/assets/tutorials/cumulus/chain-specs/rococo-custom-2-raw.json --port 30343 --ws-port 9977
+./target/release/parachain-collator --alice --collator --force-authoring --chain rococo-local-parachain-2000-raw.json --base-path /tmp/parachain/alice --port 40333 --ws-port 8844 -- --execution wasm --chain ../docs/static/assets/tutorials/cumulus/chain-specs/rococo-custom-2-raw.json --port 30343 --ws-port 9977
 
 # Submit (as sudo) parasSudoWrapper -> sudoScheduleParaInitialize(id, genesis) for parachain 2000
 # https://polkadot.js.org/apps/?rpc=ws%3A%2F%2F127.0.0.1%3A9944#/sudo
