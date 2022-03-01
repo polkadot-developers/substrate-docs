a14c7a4ce1a3d48bc8f3fdca5596ddcf689855f8
Imad Arain <arain.imad@gmail.com>
2022-03-01 17:06:46 +0100

    updating careers link

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -112,7 +112,7 @@ const MainNav = {
               },
               {
                 linkTitle: 'Careers',
-                link: `https://careers.substrate.io/jobs`,
+                link: `https://careers.substrate.io/`,
                 external: true,
               },
             ],
