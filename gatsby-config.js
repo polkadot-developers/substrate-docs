a0fd1e0560a25f54ed06e0b201d142523027fcd3
rmnprkrl <rmnprkrl@gmail.com>
2022-03-10 17:37:29 +0100

    update site meta data env

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
     terms: 'https://www.parity.io/terms',
     youtube: 'https://www.youtube.com/c/ParityTech',
     crowdcast: 'https://www.crowdcast.io/e/substrate-seminar-2',
-    substrateIO: `${process.env.GATSBY_IO_URL}`,
+    substrateIO: `${process.env.GATSBY_WEBSITE_URL}`,
   },
   flags: {
     DEV_SSR: false,
