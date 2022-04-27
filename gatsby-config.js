c8cb35d3e26a67cdf0230a3ba9ec3bbf2c844c05
jcsmillie <jonnysmillie@jonnysmillie.co.uk>
2022-04-27 13:14:12 +0100

    add basic lunr search implementation

--- gatsby-config.js
+++ gatsby-config.js
@@ -82,6 +82,47 @@ module.exports = {
         path: `${__dirname}/content/media`,
       },
     },
+    {
+      resolve: `gatsby-plugin-lunr`,
+      options: {
+        languages: [
+          // {
+          //   // ISO 639-1 language codes. See https://lunrjs.com/guides/language_support.html for details
+          //   name: 'en',
+          //   // A function for filtering nodes. () => true by default
+          //   filterNodes: node => node.frontmatter.lang === 'en',
+          //   // Add to index custom entries, that are not actually extracted from gatsby nodes
+          //   customEntries: [{ title: 'Pictures', content: 'awesome pictures', url: '/pictures' }],
+          // },
+          // {
+          //   name: 'fr',
+          //   filterNodes: node => node.frontmatter.lang === 'fr',
+          // },
+        ],
+        // Fields to index. If store === true value will be stored in index file.
+        // Attributes for custom indexing logic. See https://lunrjs.com/docs/lunr.Builder.html for details
+        fields: [
+          { name: 'title', store: true, attributes: { boost: 20 } },
+          { name: 'content' },
+          { name: 'url', store: true },
+        ],
+        // How to resolve each field's value for a supported node type
+        resolvers: {
+          // For any node of type MarkdownRemark, list how to resolve the fields' values
+          MarkdownRemark: {
+            title: node => node.frontmatter.title,
+            content: node => node.rawMarkdownBody,
+            url: node => node.fields.url,
+          },
+        },
+        //custom index file name, default is search_index.json
+        filename: 'search_index.json',
+        //custom options on fetch api call for search_ındex.json
+        fetchOptions: {
+          credentials: 'same-origin',
+        },
+      },
+    },
     /* work with images
        - need to be after sourcing images
        - docs: https://www.gatsbyjs.com/plugins/gatsby-plugin-image */
