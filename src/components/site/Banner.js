eada8305282885077024f9a03bf164b14468e0c9
Jon Eirik Boska <joneirikboska@gmail.com>
2022-06-01 10:38:57 +0200

    Make link open in new tab

--- src/components/site/Banner.js
+++ src/components/site/Banner.js
@@ -24,7 +24,11 @@ const Banner = () => {
               Edition. The 6-week virtual program offers technical workshops,
               mentorship, a prize pool of over $600,000, and an in-person Hacker
               House in NYC!
-              <a href="https://www.polkadotglobalseries.com/?utm_source=substrate.io&utm_medium=display&utm_campaign=na%20launch&utm_content=popup">
+              <a
+                href="https://www.polkadotglobalseries.com/?utm_source=substrate.io&utm_medium=display&utm_campaign=na%20launch&utm_content=popup"
+                target="_blank"
+                rel="noreferrer"
+              >
                 Register now!
               </a>
             </p>
