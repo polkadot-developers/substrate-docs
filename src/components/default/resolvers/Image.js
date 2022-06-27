72929a7075e403391c219918d375f09168e752dc
jonnysmillie <jonnysmillie@jonnysmillie.co.uk>
2022-06-27 10:59:08 +0100

    fix image alts in markdown by spreading remaining props

--- src/components/default/resolvers/Image.js
+++ src/components/default/resolvers/Image.js
@@ -4,13 +4,13 @@ import React from 'react';
 import { useImage } from '../../../hooks/use-image';
 import { testExternalLink } from '../../default/Link';
 
-function ImageResolver({ src: path }) {
+function ImageResolver({ src: path, ...others }) {
   if (testExternalLink(path)) {
-    return <img data-src={path} alt="" className="lazyload" />;
+    return <img data-src={path} className="lazyload" {...others} />;
   } else if (useImage(path)) {
     const { image } = useImage(path);
     const imageData = getImage(image.childImageSharp.gatsbyImageData);
-    return <>{imageData && <GatsbyImage image={imageData} alt="" />}</>;
+    return <>{imageData && <GatsbyImage image={imageData} {...others} />}</>;
   } else return `<Image is missing>`;
 }
 
