79240115cba227c171d021720b4c8a08d8e46483
rmnprkrl <rmnprkrl@gmail.com>
2022-06-27 17:54:20 +0200

    cleanup docs link

--- src/components/site/DocsButton.js
+++ src/components/site/DocsButton.js
@@ -4,12 +4,10 @@ import Icon from '../default/Icon';
 import { Link } from '../default/Link';
 
 export default function DocsButton() {
-  const docsLink = '/';
-
   return (
     // TODO: add docs link
     // TODO: add docs title i18n
-    <Link to={docsLink} className="block opacity-100 transform transition duration-300 ease-in-out hover:opacity-80">
+    <Link to="/" className="block opacity-100 transform transition duration-300 ease-in-out hover:opacity-80">
       <button className="flex items-center justify-center bg-substrateDark dark:bg-white text-sm py-2 w-20 rounded focus:outline-none fill-same text-white dark:text-black">
         <Icon name="docsNavIcon" className="fill-same text-substrateDark dark:text-white" />
         <span className="pl-2">Docs</span>
