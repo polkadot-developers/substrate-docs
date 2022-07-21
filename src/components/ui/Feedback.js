e735751c1dc82c84c2f8095ab052fd8a684de5ba
jonnysmillie <jonnysmillie@jonnysmillie.co.uk>
2022-07-21 12:28:49 +0200

    update submit and issue link

--- src/components/ui/Feedback.js
+++ src/components/ui/Feedback.js
@@ -6,10 +6,9 @@ import { Link } from '../default/Link';
 export default function Feedback() {
   const githubLink = 'https://github.com/substrate-developer-hub/substrate-docs/issues/new/choose';
   return (
-    <div className="w-full pt-4 pb-2 px-4 text-center bg-gray-100 mt-4 rounded">
-      <p className="text-sm m-0">Help improve the Substrate docs.</p>
+    <div className="w-full pt-2 pb-2 px-4 text-left bg-gray-100 rounded">
       <Link to={githubLink}>
-        <div className="flex items-center justify-center block mx-auto w-full text-substrateDark dark:text-substrateWhite">
+        <div className="flex items-center justify-center block mx-auto w-full text-black">
           <span className="text-sm block hover:opacity-80">Submit an issue</span>
           <Icon name="github" width="14" className="m-2 fill-current" />
         </div>
