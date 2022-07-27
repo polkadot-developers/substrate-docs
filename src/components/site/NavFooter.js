31154c1fbb85b76ee37a1cabe156cac1442f9016
jonnysmillie <jonnysmillie@jonnysmillie.co.uk>
2022-07-27 17:57:55 +0200

    footer height fix

--- src/components/site/NavFooter.js
+++ src/components/site/NavFooter.js
@@ -9,7 +9,7 @@ const NavFooter = () => {
   const { menus } = useSiteMenus();
   return (
     <nav className="m-0 p-0 hidden xs:block lg:w-full lg:max-w-screen md:h-auto">
-      <ul className="h-[640px] sm:h-auto m-0 p-0 list-none flex flex-wrap flex-col flex-start">
+      <ul className="h-[700px] sm:h-auto m-0 p-0 list-none flex flex-wrap flex-col flex-start">
         {menus.main.map(menuItem => {
           const subMenu = buildSubMenu(menus, menuItem);
           return (
